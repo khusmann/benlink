972f9f915a23953256cafbdb24647743e4eb88bd
Kyle Husmann <kyle.husmann@gmail.com>
2024-11-10 12:00:09 -0800

    add GET_PF

--- messageframe.py
+++ messageframe.py
@@ -16,6 +16,76 @@ class ReplyStatus(IntEnum):
     IN_PROGRESS = 7
 
 #################################################
+# GET_PF
+
+
+class PFActionType(IntEnum):
+    INVALID = 0
+    SHORT = 1
+    LONG = 2
+    VERY_LONG = 3
+    DOUBLE = 4
+    REPEAT = 5
+    LOW_TO_HIGH = 6
+    HIGH_TO_LOW = 7
+    SHORT_SINGLE = 8
+    LONG_RELEASE = 9
+    VERY_LONG_RELEASE = 10
+    VERY_VERY_LONG = 11
+    VERY_VERY_LONG_RELEASE = 12
+    TRIPLE = 13
+
+
+class PFEffectType(IntEnum):
+    DISABLE = 0
+    ALARM = 1
+    ALARM_AND_MUTE = 2
+    TOGGLE_OFFLINE = 3
+    TOGGLE_RADIO_TX = 4
+    TOGGLE_TX_POWER = 5
+    TOGGLE_FM = 6
+    PREV_CHANNEL = 7
+    NEXT_CHANNEL = 8
+    T_CALL = 9
+    PREV_REGION = 10
+    NEXT_REGION = 11
+    TOGGLE_CH_SCAN = 12
+    MAIN_PTT = 13
+    SUB_PTT = 14
+    TOGGLE_MONITOR = 15
+    BT_PAIRING = 16
+    TOGGLE_DOUBLE_CH = 17
+    TOGGLE_AB_CH = 18
+    SEND_LOCATION = 19
+    ONE_CLICK_LINK = 20
+    VOL_DOWN = 21
+    VOL_UP = 22
+    TOGGLE_MUTE = 23
+
+
+class PFSetting(PackedBits):
+    button_id: int = bitfield(4)
+    action: PFActionType = bitfield(4)
+    effect: PFEffectType = bitfield(8)
+
+
+class GetPFBody(PackedBits):
+    pass
+
+
+class GetPFReplyBody(PackedBits):
+    reply_status: ReplyStatus = bitfield(8)
+    # TODO: Support array bitfields
+    pf00: PFSetting = bitfield()
+    pf01: PFSetting = bitfield()
+    pf02: PFSetting = bitfield()
+    pf03: PFSetting = bitfield()
+    pf04: PFSetting = bitfield()
+    pf05: PFSetting = bitfield()
+    pf06: PFSetting = bitfield()
+    pf07: PFSetting = bitfield()
+
+#################################################
 # READ_SETTINGS / WRITE_SETTINGS
 
 
@@ -403,6 +473,8 @@ def body_disc(m: MessageFrame):
                     out = ReadSettingsReplyBody if m.is_reply else ReadSettingsBody
                 case FrameTypeBasic.WRITE_SETTINGS:
                     out = WriteSettingsReplyBody if m.is_reply else WriteSettingsBody
+                case FrameTypeBasic.GET_PF:
+                    out = GetPFReplyBody if m.is_reply else GetPFBody
                 case _:
                     out = bytes
         case FrameTypeGroup.EXTENDED:
@@ -426,6 +498,8 @@ MessageBody = t.Union[
     ReadSettingsReplyBody,
     WriteSettingsBody,
     WriteSettingsReplyBody,
+    GetPFBody,
+    GetPFReplyBody,
 ]
 
 
