39da474757ba2884d8d2bc1ed67067157ec131c0
Kyle Husmann <kyle.husmann@gmail.com>
2024-12-01 21:44:55 -0800

    add bss_settings

--- messageframe.py
+++ messageframe.py
@@ -28,8 +28,82 @@ class ReplyStatus(IntEnum):
     IN_PROGRESS = 7
 
 #################################################
-# EVENT_NOTIFICATION
+# READ_BSS_SETTINGS / WRITE_BSS_SETTINGS
 
+
+class BSSSettings(Bitfield):
+    max_fwd_times: int = bf_int(4)
+    time_to_live: int = bf_int(4)
+    ptt_release_send_location: bool
+    ptt_release_send_id_info: bool
+    ptt_release_send_bss_user_id: bool  # (Applies when BSS is turned on)
+    should_share_location: bool
+    send_pwr_voltage: bool
+    use_aprs_format: bool
+    allow_position_check: bool
+    _pad: t.Literal[0] = bf_lit_int(1, default=0)
+    aprs_ssid: int = bf_int(4)
+    _pad2: t.Literal[0] = bf_lit_int(4, default=0)
+    location_share_interval: int = bf_int(8)
+    bss_user_id: int = bf_int(32)
+    ptt_release_id_info: bytes = bf_bytes(12)
+    beacon_message: bytes = bf_bytes(18)
+    aprs_symbol: bytes = bf_bytes(2)
+    aprs_callsign: bytes = bf_bytes(6)
+
+
+class BSSSettingsExt(Bitfield):
+    bss_user_id: int = bf_int(64)
+    max_fwd_times: int = bf_int(4)
+    time_to_live: int = bf_int(4)
+    ptt_release_send_location: bool
+    ptt_release_send_id_info: bool
+    ptt_release_send_bss_user_id: bool  # (Applies when BSS is turned on)
+    should_share_location: bool
+    send_pwr_voltage: bool
+    use_aprs_format: bool
+    allow_position_check: bool
+    _pad: t.Literal[0] = bf_lit_int(1, default=0)
+    aprs_ssid: int = bf_int(4)
+    _pad2: t.Literal[0] = bf_lit_int(4, default=0)
+    location_share_interval: int = bf_int(8)
+    # bss_user_id (reordered; 32)
+    ptt_release_id_info: bytes = bf_bytes(12)
+    beacon_message: bytes = bf_bytes(18)
+    aprs_symbol: bytes = bf_bytes(2)
+    aprs_callsign: bytes = bf_bytes(6)
+    # bss_user_id_upper (reordered; 32)
+
+    _reorder = [*range(368, 368+32), *range(32, 32+32)]
+
+
+class ReadBSSSettingsBody(Bitfield):
+    unknown: int = bf_int(8)
+
+
+def bss_settings_disc(_: ReadBSSSettingsBody, __: None, n: int):
+    if n == BSSSettings.length():
+        return BSSSettings
+    if n == BSSSettingsExt.length():
+        return BSSSettingsExt
+    raise ValueError(f"Unknown size for BSSSettings ({n})")
+
+
+class ReadBSSSettingsReplyBody(Bitfield):
+    reply_status: ReplyStatus = bf_int_enum(ReplyStatus, 8)
+    bss_settings: BSSSettings | BSSSettingsExt = bf_dyn(bss_settings_disc)
+
+
+class WriteBSSSettingsBody(Bitfield):
+    bss_settings: BSSSettings | BSSSettingsExt = bf_dyn(bss_settings_disc)
+
+
+class WriteBSSSettingsReplyBody(Bitfield):
+    reply_status: ReplyStatus = bf_int_enum(ReplyStatus, 8)
+
+
+#################################################
+# EVENT_NOTIFICATION
 
 class EventNotificationType(IntEnum):
     UNKNOWN = 0
@@ -639,10 +713,10 @@ def body_disc(m: MessageFrame):
                     out = WriteSettingsReplyBody if m.is_reply else WriteSettingsBody
                 case FrameTypeBasic.GET_PF:
                     out = GetPFReplyBody if m.is_reply else GetPFBody
-#                case FrameTypeBasic.READ_BSS_SETTINGS:
-#                    out = ReadBSSSettingsReplyBody if m.is_reply else ReadBSSSettingsBody
-#                case FrameTypeBasic.WRITE_BSS_SETTINGS:
-#                    out = WriteBSSSettingsReplyBody if m.is_reply else WriteBSSSettingsBody
+                case FrameTypeBasic.READ_BSS_SETTINGS:
+                    out = ReadBSSSettingsReplyBody if m.is_reply else ReadBSSSettingsBody
+                case FrameTypeBasic.WRITE_BSS_SETTINGS:
+                    out = WriteBSSSettingsReplyBody if m.is_reply else WriteBSSSettingsBody
                 case FrameTypeBasic.EVENT_NOTIFICATION:
                     if m.is_reply:
                         raise ValueError("EventNotification cannot be a reply")
@@ -672,10 +746,10 @@ MessageBody = t.Union[
     WriteSettingsReplyBody,
     GetPFBody,
     GetPFReplyBody,
-    # ReadBSSSettingsBody,
-    # ReadBSSSettingsReplyBody,
-    # WriteBSSSettingsBody,
-    # WriteBSSSettingsReplyBody,
+    ReadBSSSettingsBody,
+    ReadBSSSettingsReplyBody,
+    WriteBSSSettingsBody,
+    WriteBSSSettingsReplyBody,
     EventNotificationBody,
 ]
 
