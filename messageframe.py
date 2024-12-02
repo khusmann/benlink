647a73afb5dea2ff4b85fcff4e78b1bff379fe8f
Kyle Husmann <kyle.husmann@gmail.com>
2024-12-01 20:01:58 -0800

    add event_notification

--- messageframe.py
+++ messageframe.py
@@ -67,11 +67,26 @@ class EventNotificationHTStatusChanged(Bitfield):
     _pad: t.Literal[0] = bf_lit_int(1, default=0)
 
 
-class EventNotificationHTStatusChangedExt(EventNotificationHTStatusChanged):
+class EventNotificationHTStatusChangedExt(Bitfield):
+    curr_channel_id: int = bf_int(8)
+    is_power_on: bool
+    is_in_tx: bool
+    is_sq: bool
+    is_in_rx: bool
+    double_channel: ChannelType = bf_int_enum(ChannelType, 2)
+    is_scan: bool
+    is_radio: bool
+    # curr_ch_id_lower (reordered; 4)
+    is_gps_locked: bool
+    is_hfp_connected: bool
+    is_aoc_connected: bool
+    _pad: t.Literal[0] = bf_lit_int(1, default=0)
     rssi: float = bf_map(bf_int(4), Scale(100 / 15))
     curr_region: int = bf_int(6)
-    curr_channel_id_upper: int = bf_int(4)
+    # curr_channel_id_upper (reordered; 4)
     _pad2: t.Literal[0] = bf_lit_int(2, default=0)
+
+    _reorder = [*range(26, 26+4), *range(8, 8+4)]
 
 
 class EventNotificationUnknown(Bitfield):
