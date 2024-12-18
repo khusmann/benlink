6e1ac3403ee5ccc70070efa07089e99bf9ecab71
Kyle Husmann <kyle.husmann@gmail.com>
2024-12-17 21:59:25 -0800

    more cleanup

--- src/benlink/protocol/rf_ch.py
+++ src/benlink/protocol/rf_ch.py
@@ -108,31 +108,3 @@ class WriteRFChBody(Bitfield):
 class WriteRFChReplyBody(Bitfield):
     reply_status: ReplyStatus = bf_int_enum(ReplyStatus, 8)
     channel_id: int = bf_int(8)
-
-
-#################################################
-# GET_DEV_STATE_VAR
-
-
-class DevStateVar(IntEnum):
-    START = 0
-    RSSI_LOW_THRESHOLD = 1
-    RSSI_HIGH_THRESHOLD = 2
-    BATTERY_LOW_THRESHOLD = 3
-    BATTERY_HIGH_THRESHOLD = 4
-    DEVICE_STATE_CHANGED = 5
-    PIO_CHANGED = 6
-    DEBUG_MESSAGE = 7
-    BATTERY_CHARGED = 8
-    CHARGER_CONNECTION = 9
-    CAPSENSE_UPDATE = 10
-    USER_ACTION = 11
-    SPEECH_RECOGNITION = 12
-    AV_COMMAND = 13
-    REMOTE_BATTERY_LEVEL = 14
-    KEY = 15
-    DFU_STATE = 16
-    UART_RECEIVED_DATA = 17
-    VMU_PACKET = 18
-
-# TODO
