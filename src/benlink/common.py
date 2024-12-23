ad056270059b0c57527e9dd557386ba9ca507517
Kyle Husmann <kyle.husmann@gmail.com>
2024-12-22 16:31:08 -0800

    documentation for common.py

--- src/benlink/common.py
+++ src/benlink/common.py
@@ -3,8 +3,12 @@ from pydantic import BaseModel, ConfigDict
 
 
 class ImmutableBaseModel(BaseModel):
+    """A base class for immutable Pydantic models"""
     model_config = ConfigDict(frozen=True)
 
 
 class DCS(t.NamedTuple):
+    """A type for setting Digital Coded Squelch (DCS) on channels"""
+
     n: int
+    """The DCS Normal (N) code"""
