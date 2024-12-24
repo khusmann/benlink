aafce70794d4d8325f3c875df9f5ef75cd01ab1c
Kyle Husmann <kyle.husmann@gmail.com>
2024-12-23 17:57:15 -0800

    update module to match README

--- src/benlink/__init__.py
+++ src/benlink/__init__.py
@@ -45,7 +45,7 @@ pip install .
 (If you are developing the package, you can use `pip install -e .` to install it
 in "editable" mode.)
 
-# Quick start
+# Quick Start
 
 First, make sure your radio is paired with your computer, and get its device
 UUID (e.g. `XX:XX:XX:XX:XX:XX`).
@@ -63,7 +63,7 @@ async def main():
 asyncio.run(main())
 ```
 
-## Next steps
+## Next Steps
 
 To see what else you can do with this library, check out the examples in the
 `benlink.client` module documentation.
