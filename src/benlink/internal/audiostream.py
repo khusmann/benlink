03ae782a2d67975930e8511fd4545929f9335a43
Kyle Husmann <kyle.husmann@gmail.com>
2025-01-16 19:34:59 -0800

    simplify rfcomm stream; improve cleanup

--- src/benlink/internal/audiostream.py
+++ src/benlink/internal/audiostream.py
@@ -4,7 +4,7 @@ import typing as t
 
 
 class SocketTask(t.NamedTuple):
-    socket: socket.socket
+    socket_handle: socket.socket
     listen_task: asyncio.Task[None]
 
 
@@ -12,9 +12,8 @@ class RfcommStream:
     _device_uuid: str
     _channel: int
     _read_size: int
-    _on_recv: t.Callable[[bytes], None]
-    _on_disconnect: t.Callable[[], None]
-    _st: SocketTask | None = None
+    _callback: t.Callable[[bytes], None]
+    _st: SocketTask | None
 
     @property
     def device_uuid(self) -> str:
@@ -31,46 +30,51 @@ class RfcommStream:
         self,
         device_uuid: str,
         channel: int,
-        on_recv: t.Callable[[bytes | None], None],
-        on_disconnect: t.Callable[[], None] = lambda: None,
+        callback: t.Callable[[bytes], None],
         read_size: int = 1024
     ):
         self._device_uuid = device_uuid
         self._channel = channel
         self._read_size = read_size
-        self._on_recv = on_recv
-        self._on_disconnect = on_disconnect
+        self._callback = callback
+        self._st = None
 
     async def connect(self, loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()):
         if self._st is not None:
             raise RuntimeError("Already connected")
 
-        skt = socket.socket(
+        socket_handle = socket.socket(
             socket.AF_BLUETOOTH,
             socket.SOCK_STREAM,
             socket.BTPROTO_RFCOMM
         )
 
-        skt.setblocking(False)
+        socket_handle.setblocking(False)
 
-        await loop.sock_connect(skt, (self._device_uuid, self._channel))
+        await loop.sock_connect(socket_handle, (self._device_uuid, self._channel))
 
         async def listen():
             while True:
-                data = await loop.sock_recv(skt, self._read_size)
-                if data:
-                    self._on_recv(data)
-                else:
-                    self._on_disconnect()
+                data = await loop.sock_recv(socket_handle, self._read_size)
+                if not data:
+                    self._st = None
                     break
+                self._callback(data)
 
         listen_task = loop.create_task(listen())
 
-        self._st = SocketTask(skt, listen_task)
+        self._st = SocketTask(socket_handle, listen_task)
 
     async def disconnect(self):
         if self._st is None:
             raise RuntimeError("Not connected")
 
         self._st.listen_task.cancel()
-        self._st.socket.close()
+        try:
+            await self._st.listen_task
+        except asyncio.CancelledError:
+            pass
+
+        self._st.socket_handle.close()
+
+        self._st = None
