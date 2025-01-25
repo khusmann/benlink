adca615cbdad4ca022e2ca9ec3d675661a194bbd
Kyle Husmann <kyle.husmann@gmail.com>
2025-01-24 18:15:50 -0800

    separate sbc_encode into function

--- src/benlink/examples/audiotransmit.py
+++ src/benlink/examples/audiotransmit.py
@@ -1,13 +1,13 @@
 import typing as t
 import sys
-import av
-import av.container
-import av.logging
 import asyncio
 import pyaudio
 from io import BytesIO
 from benlink.audio import AudioConnection
 
+import av
+from av.container import Container, InputContainer, OutputContainer
+import av.logging
 av.logging.set_level(av.logging.DEBUG)
 
 BUFFER_SIZE = 512
@@ -17,6 +17,56 @@ def print_usage():
     print("Usage: python -m benlink.examples.audiotransmit <UUID> [channel]")
     print("  <UUID>    : A valid UUID string.")
     print("  [channel] : An integer or 'auto' (default: 'auto').")
+
+
+def sbc_encode(pcm: bytes) -> bytes:
+    input_pcm: Container | None = None
+    output_sbc: Container | None = None
+
+    buffer = BytesIO()
+
+    try:
+        input_pcm = av.open(
+            BytesIO(pcm),
+            format="s16le",
+            options={
+                "ar": "32000",
+            },
+        )
+
+        assert isinstance(input_pcm, InputContainer)
+
+        output_sbc = av.open(buffer, 'w',  format="sbc")
+
+        assert isinstance(input_pcm, OutputContainer)
+
+        output_stream = output_sbc.add_stream(  # type: ignore
+            'sbc', rate=32000, options={
+                'b': "128k",
+                'sbc_delay': '0.013',
+                'msbc': 'false',
+            },
+            layout="mono",
+        )
+
+        assert isinstance(output_stream, av.AudioStream)
+
+        for frame in input_pcm.decode(audio=0):
+            packet = output_stream.encode(frame)
+            if packet:
+                output_sbc.mux(packet)
+
+        packet = output_stream.encode(None)  # Flush remaining frames
+
+        if packet:
+            output_sbc.mux(packet)
+
+        return buffer.getvalue()
+    finally:
+        if input_pcm:
+            input_pcm.close()
+        if output_sbc:
+            output_sbc.close()
 
 
 async def main(uuid: str, channel: int | t.Literal["auto"]):
@@ -36,53 +86,13 @@ async def main(uuid: str, channel: int | t.Literal["auto"]):
         await radio_audio.connect()
 
         while True:
-            chunk = mic_stream.read(BUFFER_SIZE, exception_on_overflow=False)
+            pcm = mic_stream.read(BUFFER_SIZE, exception_on_overflow=False)
 
-            print(f"Chunk size: {len(chunk)}")
+            sbc = sbc_encode(pcm)
 
-            chunk_av = av.open(
-                BytesIO(chunk),
-                format="s16le",
-                options={
-                    "ar": "32000",
-                },
-            )
+            print(f"Chunk size: {len(pcm)} -> sbc size: {len(sbc)}")
 
-            assert isinstance(chunk_av, av.container.InputContainer)
-
-            output_io = BytesIO()
-
-            output_av = av.open(
-                output_io,
-                'w',  format="sbc"
-            )
-
-            output_stream = output_av.add_stream(
-                'sbc', rate=32000, options={
-                    'b': "128k",
-                    'msbc': 'false',
-                    'sbc_delay': '0.013',
-                },
-                layout="mono",
-            )
-
-            assert isinstance(output_stream, av.AudioStream)
-
-            for f in chunk_av.decode(audio=0):
-                packet = output_stream.encode(f)
-                if packet:
-                    output_av.mux(packet)
-
-            packet = output_stream.encode(None)
-            if packet:
-                output_av.mux(packet)
-
-            chunk_av.close()
-            output_av.close()
-
-            print(f"encoded size: {len(output_io.getvalue())}")
-
-            await radio_audio.send_audio_data(output_io.getvalue())
+            await radio_audio.send_audio_data(sbc)
 
     except Exception as e:
         print(e)
